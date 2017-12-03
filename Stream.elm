71f7dd3f02a5bd69bbce5c1d9df18e0096230b0e
Jeremy Fairbank <elpapapollo@gmail.com>
2017-12-02 22:02:02 -0500

    Preparing for initial release

--- Stream.elm
+++ Stream.elm
@@ -4,9 +4,7 @@ module Stream
         , continue
         , create
         , filter
-        , foldl
         , fromList
-        , last
         , map
         , range
         , stop
@@ -14,14 +12,59 @@ module Stream
         , toList
         )
 
-
-type Element a
-    = Cons a (Stream a)
-    | Done
+{-| Fast and simple stream library for Elm. Create streams of data or flatten
+multiple operations over lists.
 
 
+# Stream Type
+
+@docs Stream
+
+
+# Use Streams with Lists
+
+@docs fromList, toList
+
+
+# Create Streams
+
+@docs range, create, continue, stop
+
+
+# Transform Streams
+
+@docs map, filter, take
+
+-}
+
+
+{-| Represent a stream with a thunk. Streams let you efficiently and lazily
+transform values. Streams flatten operations like [`map`](#map) and
+[`filter`](#filter) so you don't have to iterate through a stream multiple
+times like a list.
+
+    Stream.range 1 1000
+        |> Stream.map (\n -> n * 2)
+        |> Stream.filter (\n -> n > 10)
+        |> Stream.take 3
+        |> Stream.toList == [12, 14, 16]
+
+You can use streams with lists of data too.
+
+    [1, 2, 3, 4, 5]
+        |> Stream.fromList
+        |> Stream.map (\n -> 2 ^ n)
+        |> Stream.filter (\n -> n > 8)
+        |> Stream.toList == [16, 32]
+
+-}
 type alias Stream a =
-    () -> Element a
+    () -> StreamElement a
+
+
+type StreamElement a
+    = Cons a (Stream a)
+    | Nil
 
 
 type Next a
@@ -29,76 +72,164 @@ type Next a
     | Stop
 
 
-continue : a -> Next a
-continue =
-    Continue
+empty : Stream a
+empty () =
+    Nil
 
 
-stop : Next a
-stop =
-    Stop
+{-| Create a stream from a list.
+
+    fromList [1, 2, 3]
+
+-}
+fromList : List a -> Stream a
+fromList list () =
+    case list of
+        [] ->
+            Nil
+
+        x :: xs ->
+            Cons x (fromList xs)
 
 
+{-| Convert a stream into a list. Evaluates every value in a stream. Be careful
+with infinite streams, so use [`take`](#take) before calling.
+
+    [1, 2, 3]
+        |> fromList
+        |> toList == [1, 2, 3]
+
+-}
+toList : Stream a -> List a
+toList stream =
+    let
+        toList_ : StreamElement a -> List a -> List a
+        toList_ streamElement list =
+            case streamElement of
+                Cons value nextStream ->
+                    toList_ (nextStream ()) (value :: list)
+
+                Nil ->
+                    list
+    in
+        []
+            |> toList_ (stream ())
+            |> List.reverse
+
+
+{-| Create a stream of numbers with each number increasing by one. Provide the
+starting and ending number as arguments.
+
+    range 1 10
+        |> toList == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    range 5 1
+        |> toList == [5, 4, 3, 2, 1]
+
+-}
+range : Int -> Int -> Stream Int
+range start end =
+    if start > end then
+        empty
+    else
+        create
+            (\n ->
+                if n < end then
+                    continue (n + 1)
+                else
+                    stop
+            )
+            start
+
+
+{-| Create a stream with a generator function to produce
+each value based on the previous value. Return the next value wrapped in
+[`continue`](#continue) or stop the stream by returning [`stop`](#stop).
+Provide the initial value as the second argument to `create`.
+
+    naturalNumbers =
+        create (\n -> continue (n + 1)) 1
+
+    upTo5 =
+        create
+            (\n ->
+                if n < 5 then
+                    continue (n + 1)
+                else
+                    stop
+            )
+            1
+
+-}
 create : (a -> Next a) -> a -> Stream a
 create generator initialValue () =
     let
+        create_ : Next a -> StreamElement a
         create_ next =
             case next of
                 Continue value ->
                     Cons value (\() -> create_ (generator value))
 
                 Stop ->
-                    Done
+                    Nil
     in
         create_ (continue initialValue)
 
 
-range : Int -> Int -> Stream Int
-range start end =
-    create
-        (\n ->
-            if n <= end then
-                continue (n + 1)
-            else
-                stop
-        )
-        start
+{-| Used with [`create`](#create) to continue the stream with a new value.
+
+    naturalNumbers =
+        create (\n -> continue (n + 1)) 1
+
+-}
+continue : a -> Next a
+continue =
+    Continue
 
 
-fromList : List a -> Stream a
-fromList list () =
-    case list of
-        [] ->
-            Done
+{-| Used with [`create`](#create) to stop the stream.
 
-        x :: xs ->
-            Cons x (fromList xs)
+    upTo5 =
+        create
+            (\n ->
+                if n < 5 then
+                    continue (n + 1)
+                else
+                    stop
+            )
+            1
 
-
-foldl : (item -> memo -> memo) -> memo -> Stream item -> Stream memo
-foldl f memo stream () =
-    case stream () of
-        Cons value nextStream ->
-            let
-                newMemo =
-                    f value memo
-            in
-                Cons newMemo (foldl f newMemo nextStream)
-
-        Done ->
-            Done
+-}
+stop : Next a
+stop =
+    Stop
 
 
+{-| Transform every value in a stream with a function.
+
+    [1, 2, 3]
+        |> fromList
+        |> map (\n -> n * 2)
+        |> toList == [2, 4, 6]
+
+-}
 map : (a -> b) -> Stream a -> Stream b
 map f stream () =
     case stream () of
         Cons value nextStream ->
             Cons (f value) (map f nextStream)
 
-        Done ->
-            Done
+        Nil ->
+            Nil
 
 
+{-| Keep values that return `True` for the provided function.
+
+    range 1 10
+        |> filter (\n -> n > 7)
+        |> toList == [8, 9, 10]
+
+-}
 filter : (a -> Bool) -> Stream a -> Stream a
 filter f stream () =
     case stream () of
@@ -108,48 +239,29 @@ filter f stream () =
             else
                 filter f nextStream ()
 
-        Done ->
-            Done
+        Nil ->
+            Nil
 
 
+{-| Take only up to *n* values from the stream. Useful for consuming infinite
+streams.
+
+    naturalNumbers =
+        create (\n -> continue (n + 1)) 1
+
+    naturalNumbers
+        |> take 3
+        |> toList == [1, 2, 3]
+
+-}
 take : Int -> Stream a -> Stream a
 take n stream () =
     if n <= 0 then
-        Done
+        Nil
     else
         case stream () of
-            Done ->
-                Done
-
             Cons value nextStream ->
                 Cons value (take (n - 1) nextStream)
 
-
-last : Stream a -> Maybe a
-last stream =
-    let
-        last_ stream accum =
-            case stream () of
-                Cons value nextStream ->
-                    last_ nextStream (Just value)
-
-                Done ->
-                    accum
-    in
-        last_ stream Nothing
-
-
-toList : Stream a -> List a
-toList stream =
-    let
-        toList_ streamValue acc =
-            case streamValue of
-                Done ->
-                    acc
-
-                Cons value nextStream ->
-                    toList_ (nextStream ()) (value :: acc)
-    in
-        []
-            |> toList_ (stream ())
-            |> List.reverse
+            Nil ->
+                Nil
---
+++ tests/StreamTest.elm
@@ -0,0 +1,89 @@
+module StreamTest exposing (..)
+
+import Stream
+import Expect exposing (Expectation)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Stream"
+        [ describe ".fromList"
+            [ test "creates a stream from a list" <|
+                \_ ->
+                    [ 1, 2, 3 ]
+                        |> Stream.fromList
+                        |> Stream.toList
+                        |> Expect.equal [ 1, 2, 3 ]
+            , test "creates empty stream from empty list" <|
+                \_ ->
+                    []
+                        |> Stream.fromList
+                        |> Stream.toList
+                        |> Expect.equal []
+            ]
+        , describe ".range"
+            [ test "creates an incrementing stream from a starting to ending number" <|
+                \_ ->
+                    Stream.range 1 5
+                        |> Stream.toList
+                        |> Expect.equal [ 1, 2, 3, 4, 5 ]
+            , test "creates an empty stream if starting is greater than ending" <|
+                \_ ->
+                    Stream.range 5 1
+                        |> Stream.toList
+                        |> Expect.equal []
+            , test "creates a single element stream when starting and ending are same" <|
+                \_ ->
+                    Stream.range 42 42
+                        |> Stream.toList
+                        |> Expect.equal [ 42 ]
+            , test "creates a stream over negative and positive numbers" <|
+                \_ ->
+                    Stream.range -2 2
+                        |> Stream.toList
+                        |> Expect.equal [ -2, -1, 0, 1, 2 ]
+            ]
+        , describe ".create"
+            [ test "creates a custom stream" <|
+                \_ ->
+                    let
+                        stream =
+                            Stream.create
+                                (\n ->
+                                    if n < 5 then
+                                        Stream.continue (n + 1)
+                                    else
+                                        Stream.stop
+                                )
+                                1
+                    in
+                        stream
+                            |> Stream.toList
+                            |> Expect.equal [ 1, 2, 3, 4, 5 ]
+            ]
+        , describe ".map"
+            [ test "transforms every value with a function" <|
+                \_ ->
+                    Stream.range 1 3
+                        |> Stream.map ((*) 2)
+                        |> Stream.toList
+                        |> Expect.equal [ 2, 4, 6 ]
+            ]
+        , describe ".filter"
+            [ test "only keeps values that return true" <|
+                \_ ->
+                    Stream.range 1 10
+                        |> Stream.filter (\n -> n > 5)
+                        |> Stream.toList
+                        |> Expect.equal [ 6, 7, 8, 9, 10 ]
+            ]
+        , describe ".take"
+            [ test "only takes the first n values" <|
+                \_ ->
+                    Stream.range 1 10
+                        |> Stream.take 5
+                        |> Stream.toList
+                        |> Expect.equal [ 1, 2, 3, 4, 5 ]
+            ]
+        ]
